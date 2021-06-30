3809ea9da957959d76e0e79ee9d45be9185a1f16
inseop0928 <54462639+inseop0928@users.noreply.github.com>
2021-06-30 11:11:54 +0900

    사업장번호 추가

--- Regex.sql
+++ Regex.sql
@@ -1,2 +1,5 @@
 --전화번호
 SELECT REGEXP_REPLACE('tel_no', '(02|.{3})(.+)(.{4})', '\1-\2-\3') tel_no2 FROM DUAL; 
+
+--사업자번호
+SELECT REGEXP_REPLACE('BIZ_NO', '(.{3})(.{2})(.{5})', '\1-\2-\3') BIZ_NO FROM DUAL;
