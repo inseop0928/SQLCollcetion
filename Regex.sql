d108984be406bd2cbbbc1bbc7729ff7eaebb09fe
inseop0928 <54462639+inseop0928@users.noreply.github.com>
2021-10-21 17:28:31 +0900

    포맷적용 컬럼 검색 추가

--- Regex.sql
+++ Regex.sql
@@ -3,3 +3,5 @@ SELECT REGEXP_REPLACE('tel_no', '(02|.{3})(.+)(.{4})', '\1-\2-\3') tel_no2 FROM 
 
 --사업자번호
 SELECT REGEXP_REPLACE('BIZ_NO', '(.{3})(.{2})(.{5})', '\1-\2-\3') BIZ_NO FROM DUAL;
+--사업자번호 포맷적용검색
+SELECT * FROM DUAL WHERE REGEXP_LIKE(BIZ_NO, '[0-9]{3}\-[0-9]{2}\-[0-9]{5}') AND LENGTH(BIZ_NO) = 12
